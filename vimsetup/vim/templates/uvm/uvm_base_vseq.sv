bdd3e0c1ec742d819d60d16244f42f88f62dccd8
Matt Li <mattyhli@gmail.com>
2021-05-11 05:03:54 +0000

    fix typo

--- vimsetup/vim/templates/uvm/uvm_base_vseq.sv
+++ vimsetup/vim/templates/uvm/uvm_base_vseq.sv
@@ -1,7 +1,7 @@
 `ifndef _{:UPPERNAME:}_BASE_SEQ__SV_
 `define _{:UPPERNAME:}_BASE_SEQ__SV_
 
-class {:NAME:}_base_seq extends uvm_sequnece;
+class {:NAME:}_base_seq extends uvm_sequence;
     `uvm_object_utils({:NAME:}_base_seq)
 
     `uvm_declare_p_sequencer({:NAME:}_vsqr)
